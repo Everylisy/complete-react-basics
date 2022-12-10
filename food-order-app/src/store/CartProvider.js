fbda0e34aa5ee25d714de23f7f87b71e396f1ca7
violetwhenisawu <anashape710@gmail.com>
2022-12-11 01:11:45 +0900

    feat: 장바구니 항목 추가 state를 관리할 reducer 로직 작성

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const initCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedAmount =
+      state.totalAmount + action.item.amount * action.item.price;
+    return {
+      items: updatedItems,
+      totalAmount: updatedAmount,
+    };
+  }
+};
+
 function CartProvider(props) {
-  const addCartItemHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    initCartState
+  );
+
+  const addCartItemHandler = (item) => {
+    dispatchCartAction({ type: 'ADD_ITEM', item: item });
+  };
 
   const removeCartItemHandler = (id) => {};
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addCartItemHandler,
     removeItem: removeCartItemHandler,
   };
