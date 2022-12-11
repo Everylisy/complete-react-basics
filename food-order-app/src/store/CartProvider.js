d19dcc499db637ee8e51c1ec060ec5aaafd4db72
violetwhenisawu <anashape710@gmail.com>
2022-12-11 19:48:56 +0900

    style: 헷갈리는 로직 주석 추가

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -11,18 +11,26 @@ const cartReducer = (state, action) => {
     const updatedAmount =
       state.totalAmount + action.item.amount * action.item.price;
 
+    // 이미 장바구니에 있는 항목 index 값
     const existItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
+
+    // 이미 장바구니에 있는 항목
     const existItem = state.items[existItemIndex];
     let updatedItems;
 
+    // 이미 장바구니에 있는 항목이 있으면,
     if (existItem) {
+      // 이전 항목에 새로 dispatch된 수량만 업데이트
       const updatedItem = {
         ...existItem,
         amount: existItem.amount + action.item.amount,
       };
+
+      // 원래 state에 들어있던 항목 복사
       updatedItems = [...state.items];
+      // 이미 state에 있는 항목의 인덱스에 접근,수량만 업데이트 한 객체로 오버라이드
       updatedItems[existItemIndex] = updatedItem;
     } else {
       updatedItems = state.items.concat(action.item);
