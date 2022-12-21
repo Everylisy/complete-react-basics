4baf6f700993b9625daa34b2b0b792b386081bd8
violetwhenisawu <anashape710@gmail.com>
2022-12-22 01:46:02 +0900

    feat: http 요청 에러핸들링 코드 추가

--- 04-sending-http-requests/src/App.js
+++ 04-sending-http-requests/src/App.js
@@ -8,20 +8,30 @@ const BASE_URL = 'https://swapi.dev/api/';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMovies() {
     setIsLoading(true);
-    const res = await fetch(`${BASE_URL}films`);
-    const data = await res.json();
-    const newData = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_date,
-        openingText: movieData.opening_crawl,
-      };
-    });
-    setMovies(newData);
+    setError(null);
+
+    try {
+      const res = await fetch(`${BASE_URL}films`);
+
+      if (!res.ok) throw new Error('Something went wrong⛔');
+
+      const data = await res.json();
+      const newData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl,
+        };
+      });
+      setMovies(newData);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   }
 
@@ -32,7 +42,10 @@ function App() {
       </section>
       <section>
         {!isLoading && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No movie data found</p>}
+        {!isLoading && movies.length === 0 && !error && (
+          <p>No movie data found.</p>
+        )}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
